b9120bd3f2e0c43f22cf63c9e6e87439cc58efa4
AlbertHdez <mariohdez_proyectos@outlook.com>
2021-04-19 16:41:55 -0500

    Employee Register

--- OOPClass.cpp
+++ OOPClass.cpp
@@ -1,116 +1,96 @@
-/*
-    Practica 2:
-        - Generar un tipo de dato llamado tiempo, deberá tener dos campos: minutos y horas.
-        - El programa deberá mostrar la suma de dos tiempos dados por el usuario.
-*/
+// Class-19-04-21.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
 
 #include <iostream>
-#include <string>
-#include <regex>
-#include <sstream>
+#include <cstdlib>
+#include <time.h>
 
-struct Time {
-    int hours, minutes;
+struct Date {
+    int day, month, year;
 };
 
-/*
-*   getTime Function -> (Time time)
-*       - Get a time in string
-*       - Get the hours: no more than 23 hrs or set 0
-*       - Get the minutes: if it's more or equal than 60, add 1 hour and rest 60 minutes
-*       - Return a Time with the hours and minutes
-*/
-Time getTime(std::string time) {
-    int hours, minutes;
-    std::smatch m;
-    std::regex_search(time, m, std::regex("^[0-9]{1,2}"));
-    hours = std::stoi(m[0]) % 24;
-    std::regex_search(time, m, std::regex("[0-9]{1,2}$"));
-    if (std::stoi(m[0]) <= 60) {
-        minutes = std::stoi(m[0]);
+struct Employee {
+    int id;
+    char name[10];
+    Date date;
+    float salary[3];
+};
+
+Employee getEmployeeInput() {
+    Employee employee;
+    Date date;
+
+    std::cout << "Ingresa el id: ";
+    std::cin >> employee.id;
+
+    std::cout << "Ingresa el nombre: ";
+    std::cin >> employee.name;
+
+    //std::cout << "\nIngresa fecha del registro" << std::endl;
+    //std::cout << "Ingresa Dia: ";
+    //std::cin >> date.day;
+    //std::cout << "Ingresa Mes: ";
+    //std::cin >> date.month;
+    //std::cout << "Ingresa Año: ";
+    //std::cin >> date.year;
+    employee.date = {
+        std::rand() % 30 + 1,
+        std::rand() % 12 + 1,
+        std::rand() % 32 + 1990
+    };
+
+    //std::cout << "\nIngresa los sueldos" << std::endl;
+    //for (int i = 0; i < 3; i++) {
+    //    std::cout << "Ingresa salario " << i << ": ";
+    //    std::cin >> employee.salary[i];
+    //}
+
+    for (int i = 0; i < 3; i++) {
+        employee.salary[i] = (std::rand() % 100) * 100 + (std::rand() % 100) + (std::rand() % 100) * 0.01;
     }
-    else {
-        hours++;
-        minutes = std::stoi(m[0]) - 60;
-    }
-    return Time{ hours, minutes };
+
+    return employee;
 }
 
-
-/*
-*   sumTime Function -> (Time time)
-*       - Get 2 Times
-*       - Sum the hours: get the module of 24
-*       - Sum the minutes: if it's more or equal to 60, add 1 hour and rest 60 minutes
-*       - Return a Time with the hours and minutes
-*/
-Time sumTime(Time a, Time b) {
-    int hours, minutes;
-    hours = (a.hours + b.hours) % 24;
-    minutes = a.minutes + b.minutes;
-
-    if (minutes >= 60) {
-        hours++;
-        minutes -= 60;
+void printEmployeeData(Employee employee) {
+    std::cout << "ID:         " << employee.id << std::endl;
+    std::cout << "Nombre:     " << employee.name << std::endl;
+    std::cout << "Registro:   " << employee.date.day << "/" << employee.date.month << "/" << employee.date.year << std::endl;
+    for (int i = 0; i < 3; i++) {
+        std::cout << "Salario " << i + 1 << ":  $" << employee.salary[i] << std::endl;
     }
-
-    return Time{ hours, minutes };
-}
-
-/*
-*   timeStr Function -> (std::string string)
-*       - Get a Time
-*       - Format hours: if it's minus than 10, add the 1st zero
-*       - Format minutes: if it's mines than 10, add the 1st zero
-*       - Format time
-*       - Return a string with the time in hh:mm format
-*/
-std::string timeStr(Time time) {
-    std::stringstream ss;
-    std::string hours, minutes;
-
-    if (time.hours < 10) {
-        ss << "0" << time.hours;
-        hours = ss.str();
-        ss.str("");
-    }
-    else {
-        hours = std::to_string(time.hours);
-    }
-
-    if (time.minutes < 10) {
-        ss << "0" << time.minutes;
-        minutes = ss.str();
-        ss.str("");
-    }
-    else {
-        minutes = std::to_string(time.minutes);
-    }
-
-    ss << hours << ":" << minutes;
-
-    return ss.str();
 }
 
 int main()
 {
-    Time timeA, timeB, timeC;
-    std::string time;
+    Employee employees[2];
+    // std::cout << "===== Primer Empleado =====" << std::endl;
+    // e1 = getEmployeeInput();
+    // std::cout << "\n===== Segundo Empleado =====" << std::endl;
+    // e2 = getEmployeeInput(); 
 
-    std::cout << "Ingresa la primera hora (hh:mm): ";
-    std::cin >> time;
-    timeA = getTime(time); // Converts string to Time struct
+    std::srand(time(0));
 
-    std::cout << "Ingresa la segunda hora (hh:mm): ";
-    std::cin >> time;
-    timeB = getTime(time);
+    for (int i = 0; i < 2; i++) {
+        std::cout << "======= Empleado #" << i << " =======" << std::endl;
+        employees[i] = getEmployeeInput();
+        std::cout << std::endl;
+    }
 
-    timeC = sumTime(timeA, timeB); // Sums 2 Times struct
+    std::cout << "====== Empleados ======" << std::endl;
+    for (int i = 0; i < 2; i++) {
+        printEmployeeData(employees[i]);
+        if (i < 1) std::cout << std::endl;
+    }
+    std::cout << "=======================" << std::endl;
 
-    std::cout << "\nLa hora es " << timeStr(timeC) << std::endl; // Converts and Get a string from a Time struct
-
-    std::cout << "\nUn programa de Mario Hdez (~^u^)~" << std::endl;
-
+    //std::cout << "===== Empleados =====" << std::endl;
+    //printEmployeeData(e1);
+    //std::cout << std::endl;
+    //printEmployeeData(e2);
+    //std::cout << "=====================" << std::endl;
     system("pause");
+    return 0;
 }
+
 
