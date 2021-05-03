93cb7d1bb2629834192261680666ed5c65e3ae3a
AlbertHdez <mariohdez_proyectos@outlook.com>
2021-05-03 10:05:45 -0500

    Playing with pointers

--- OOPClass.cpp
+++ OOPClass.cpp
@@ -1,23 +1,54 @@
 #include<iostream>
 
 int main() {
+	int x = 3, y = 7, z[5] = { 2, 4, 6, 8, 10 };
+	printf("\nx= %d \ty= %d \tz[0]= %d \tz[1]= %d \tz[2]= %d \tz[3]= %d \tz[4]= %d \t",
+				x, y, z[0], z[1], z[2], z[3], z[4]);
 
-	int num[] = { 56, 75, 22, 18, 90 };
-	int* ptr = num;
+	// First
+	int *IX;
+	IX = &x;
+	*IX = 1;
+	printf("\nx= %d \ty= %d \tz[0]= %d \tz[1]= %d \tz[2]= %d \tz[3]= %d \tz[4]= %d \t",
+		x, y, z[0], z[1], z[2], z[3], z[4]);
 
-	std::cout << "Valor de ptr: " << *ptr << std::endl;
-	ptr++;
-	std::cout << "Valor de ptr++: " << *ptr << std::endl;
-	ptr--;
-	std::cout << "Valor de ptr--: " << *ptr << std::endl;
-	ptr += 2;
-	std::cout << "Valor de ptr+2: " << *ptr << std::endl;
-	ptr -= 1;
-	std::cout << "Valor de ptr-1: " << *ptr << std::endl;
-	ptr += 3;
-	std::cout << "Valor de ptr+3: " << *ptr << std::endl;
-	ptr -= 2;
-	std::cout << "Valor de ptr-2: " << *ptr << std::endl;
+	// Second
+	IX = &z[2];
+	y = *IX;
+	*IX = 15;
+	printf("\nx= %d \ty= %d \tz[0]= %d \tz[1]= %d \tz[2]= %d \tz[3]= %d \tz[4]= %d \t",
+		x, y, z[0], z[1], z[2], z[3], z[4]);
+
+	// Third
+	x = *IX + 5;
+	*IX = *IX - 5;
+	printf("\nx= %d \ty= %d \tz[0]= %d \tz[1]= %d \tz[2]= %d \tz[3]= %d \tz[4]= %d \t",
+		x, y, z[0], z[1], z[2], z[3], z[4]);
+
+	// Fourth
+	++ *IX;
+	*IX += 1;
+	printf("\nx= %d \ty= %d \tz[0]= %d \tz[1]= %d \tz[2]= %d \tz[3]= %d \tz[4]= %d \t",
+		x, y, z[0], z[1], z[2], z[3], z[4]);
+
+	// Fifth
+	x = *(IX + 1);
+	y = *IX;
+	printf("\nx= %d \ty= %d \tz[0]= %d \tz[1]= %d \tz[2]= %d \tz[3]= %d \tz[4]= %d \t",
+		x, y, z[0], z[1], z[2], z[3], z[4]);
+
+	// Sixth
+	IX = IX + 4;
+	y = *IX;
+	printf("\nx= %d \ty= %d \tz[0]= %d \tz[1]= %d \tz[2]= %d \tz[3]= %d \tz[4]= %d \t",
+		x, y, z[0], z[1], z[2], z[3], z[4]);
+
+	//Seventh
+	IX = &x;
+	IX += 1;
+	x = *IX;
+	printf("\nx= %d \ty= %d \tz[0]= %d \tz[1]= %d \tz[2]= %d \tz[3]= %d \tz[4]= %d \t",
+		x, y, z[0], z[1], z[2], z[3], z[4]);
 
 	return 0;
 }
