e4264bbcbfbe6a21a9fc9869a92dfdb738406ec4
AlbertHdez <mariohdez_proyectos@outlook.com>
2021-06-08 08:39:10 -0500

    Class & Methods: A and B

--- OOPClass.cpp
+++ OOPClass.cpp
@@ -1,23 +1,55 @@
 #include<iostream>
 
-class Point {
+class B;
+
+class A {
 	private:
-		float x, y;
+		int value;
 	public:
-		Point() :x(0), y(0) {}
-		Point(float x) : x(x), y(0) {}
-		Point(float x, float y) : x(x), y(y) {}
+		void setValue(int);
+		void show();
+		friend int sum(A, B);
 };
 
+void A::setValue(int value) { this->value = value; }
+void A::show() { std::cout << "El valor de A es: " << this->value << "\n"; }
+
+class B {
+private:
+	int value;
+public:
+	void setValue(int);
+	void show();
+	friend int sum(A, B);
+};
+
+void B::setValue(int value) { this->value = value; }
+void B::show() { std::cout << "El valor de B es: " << this->value << "\n"; }
+
+// Friend Functions
+int sum(A a, B b) {
+	return a.value + b.value;
+}
+
 int main() {
+	A a;
+	B b;
+	int value;
 
-	Point triangle[3] = {
-		Point(),
-		Point(10),
-		Point(5, 10)
-	};                  
+	// Get Values
+	std::cout << "Ingresa el valor de A: ";
+	std::cin >> value;
+	a.setValue(value);
+	               
+	std::cout << "Ingresa el valor de B: ";
+	std::cin >> value;
+	b.setValue(value);
 
+	value = sum(a, b);
 
+	std::cout << "La suma de A y B es: " << value << "\n";
+
+	system("pause");
 
 	return 0;
 }
